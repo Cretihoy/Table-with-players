9f4c4ba64b803a8a32e89ce6bc319b12ef63c8d7
cretihoy <sergzhio238@gmail.com>
2023-03-22 17:11:30 +0300

    add fun for count the numbers

--- app/src/main/java/com/example/testtask/MainActivity.kt
+++ app/src/main/java/com/example/testtask/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.testtask
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
+
+    private val rootLayout: TableLayout by lazy { findViewById(R.id.root) }
+    private var tableRowList = listOf<TableRow>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initRows()
+        initEditListeners()
+    }
+
+    private fun initRows() {
+        tableRowList = rootLayout.getChildren()
+    }
+
+    private fun initEditListeners() {
+        tableRowList.forEach { row ->
+            row.getChildren<EditText>()
+                .forEach { edit ->
+                    edit.addTextChangedListener {
+                        calculateScore()
+                    }
+                }
+        }
+    }
+
+    private fun calculateScore() {
+        rootLayout.getChildren<TableRow>()
+            .forEach { calculateRowScore(it) }
+    }
+
+    private fun calculateRowScore(row: TableRow) {
+        var scoreValue = 0
+        row.getChildren<EditText>()
+            .forEach {
+                val editNumber = it.text.toString().toIntOrNull() ?: 0
+                scoreValue += editNumber
+            }
+        val scoreView = row.getChildren<TextView>().filter { it.tag == "score" }.first()
+        scoreView.text = scoreValue.toString()
+    }
+
+    private fun calculatePlace() {
+
+    }
+
+    inline fun <reified T> ViewGroup.getChildren(): List<T> {
+        return children
+            .filter { it is T }
+            .map { it as T }
+            .toList()
     }
 }
